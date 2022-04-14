710dc156ddcb7f496a3e6c74bb03c930dc76d380
leandro wagner <leanw1711@gmail.com>
2022-04-14 18:48:02 -0300

    correcciones

--- main.js
+++ main.js
@@ -6,35 +6,31 @@ class Usuario{
         this.mascotas = mascotas
     }
     getFullName(){
-        console.log(`${this.nombre} ${this.apellido}`);
+        return `${this.nombre} ${this.apellido}`;
     }
     addMascota(name){
         this.mascotas.push(name);
-        return console.log(this.mascotas)
     }
     countMascotas(){
-        console.log(this.mascotas.length);
+        return this.mascotas.length;
     }
     addBokk(name,autor){
         this.libros.push({nombre:name,autor:autor});
-        return console.log(this.libros);
     }
     getBookNames(){
         const nameBook = this.libros.map(book=>book.nombre);
-        return console.log(nameBook);
+        return nameBook;
     }
 }
 
 const usuario = new  Usuario('Leandro','Wagner',[],[]);
 
-usuario.getFullName();
-// usuario.addMascota('pajaro');
-usuario.addMascota('perro');
-// usuario.addMascota('gato');
-usuario.countMascotas();
-// usuario.addBokk('El Alquimista','Paulo Coelho');
+console.log(usuario.getFullName());
+usuario.addMascota('pajaro');
+usuario.addMascota('gato');
+console.log(usuario.countMascotas());
+usuario.addBokk('El Alquimista','Paulo Coelho');
 usuario.addBokk('Martin Fierro','Jose Hernndez');
-// console.log(usuario);
-usuario.getBookNames();
+console.log(usuario.getBookNames());
 
 
